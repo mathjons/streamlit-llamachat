d80948380dc88029471c019cadd38075917a4223
traxyl <42194936+traxyl@users.noreply.github.com>
2023-11-01 20:11:17 +0000

    a

--- Hello.py
+++ Hello.py
@@ -1,17 +1,3 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
@@ -74,7 +60,6 @@ def load_data():
         return index
 
 index = load_data()
-
 
 chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
